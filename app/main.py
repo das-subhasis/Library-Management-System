462432bcae80425483ceb6d3b6e7f46cfcb06c5b
subhasis <das.shubasis2003@gmail.com>
2024-11-13 17:01:27 +0530

    Added Routes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.api.v1.core import lifespan
-
+from app.api.v1.routes import LibraryRoute
 
 app = FastAPI(
     title="Library Management API (ver. 1.0)",
     lifespan=lifespan
 )
+
+############################################################
+app.include_router(LibraryRoute)
+############################################################
 
 
 @app.get('/')
